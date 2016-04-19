24a1e3af749c4035312e41a4958a30516cacff42
ratscrew <ratscrew2002@yahoo.com>
2016-04-19 14:45:19 +0100

    add momgo db connect promiss

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
+"use strict";
+var mongoDriver = require('mongodb');
+var q = require('q');
+var MomGo = (function () {
+    function MomGo(serverName, dbName) {
+        this.serverName = serverName;
+        this.dbName = dbName;
+        this.MongoClient = mongoDriver.MongoClient;
+        this.OnjectID = mongoDriver.ObjectID;
+        this.dbs = {};
+        this.db();
+    }
+    MomGo.prototype.mongoConnection = function (connectionStr) {
+        var me = this;
+        if (me.dbs[connectionStr]) {
+            return q.when(me.dbs[connectionStr]);
+        }
+        else {
+            var deferred = q.defer();
+            me.dbs[connectionStr] = deferred.promise;
+            console.log({ connectionStr: connectionStr });
+            me.MongoClient.connect(connectionStr, function (err, _db) {
+                if (err) {
+                    console.log({ connectionError: err, connectionStr: connectionStr });
+                    //throw new Error(err);
+                    return deferred.reject(err);
+                }
+                _db.on('disconnect', function (err) {
+                    console.log({ disconnect: err });
+                    if (me.dbs[connectionStr])
+                        delete me.dbs[connectionStr];
+                    throw new Error(err);
+                });
+                me.dbs[connectionStr] = _db;
+                deferred.resolve(_db);
+            });
+            return me.dbs[connectionStr];
+        }
+    };
+    MomGo.prototype.db = function (_dbName) {
+        var me = this;
+        if (!_dbName || _dbName == "default")
+            _dbName = me.dbName;
+        var connectionStr = "";
+        if (me.serverName.indexOf('mongodb://') == -1)
+            connectionStr = 'mongodb://' + me.serverName + ':27017/' + _dbName;
+        else {
+            connectionStr = me.serverName.replace("27017?", '27017/' + _dbName + "?");
+        }
+        return me.mongoConnection(connectionStr).catch(function (err) {
+            console.log({ connectionStr: connectionStr, err: err });
+            console.log('trying again');
+            return me.mongoConnection(connectionStr);
+        });
+    };
+    return MomGo;
+}());
+exports.MomGo = MomGo;
 //# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,62 @@
+import {server, publicFunction,globalEventHandler,globalEvent} from 'rx-server';
+import * as mongoDriver from 'mongodb';
+import * as q from 'q';
+
+export class MomGo {
+    MongoClient = mongoDriver.MongoClient;
+    OnjectID = mongoDriver.ObjectID;
+    private dbs = {};
+    
+    constructor(public serverName, public dbName) {
+        this.db()
+    }
+    
+    mongoConnection(connectionStr){
+        let me = this;
+        if(me.dbs[connectionStr]){
+            return q.when(me.dbs[connectionStr])
+        }
+        else{
+            var deferred = q.defer();
+            me.dbs[connectionStr] = deferred.promise;
+            console.log({connectionStr:connectionStr});
+            me.MongoClient.connect(connectionStr, (err, _db)=> {
+                if(err) {
+                    console.log({connectionError:err, connectionStr:connectionStr});
+                    //throw new Error(err);
+                    return deferred.reject(err);
+                    //log(err);
+                }
+
+                _db.on('disconnect', function (err) {
+                    console.log({disconnect:err});
+                    if(me.dbs[connectionStr]) delete me.dbs[connectionStr];
+                    throw new Error(err);
+                });
+
+                me.dbs[connectionStr] = _db;
+
+                deferred.resolve(_db);
+            });
+            return me.dbs[connectionStr];
+        }
+    }
+    
+    
+    db(_dbName?):Q.IPromise<mongoDriver.Db>{
+        let me = this;
+        if(!_dbName || _dbName == "default") _dbName = me.dbName;
+        let connectionStr:string = "";
+        if(me.serverName.indexOf('mongodb://') == -1) connectionStr = 'mongodb://' + me.serverName + ':27017/' + _dbName;
+        else {
+            connectionStr = me.serverName.replace("27017?",'27017/' + _dbName + "?") ;
+        }
+        return me.mongoConnection(connectionStr).catch(function (err) {
+            console.log({connectionStr:connectionStr,err:err});
+            console.log('trying again');
+            return me.mongoConnection(connectionStr)
+        })
+    }
+    
+}
+
