3393505b80ec912b7e29e8135be8294294726979
ratscrew <ratscrew2002@yahoo.com>
2016-05-24 14:46:19 +0100

    update rx-server

--- clientScripts/momgo.js
+++ clientScripts/momgo.js
@@ -73,7 +73,7 @@ System.register(['rxjs/rx'], function(exports_1, context_1) {
                                     }
                             }
                         }
-                    }).inspectTime(200);
+                    }).debounceTime(50);
                     return rx_1.Observable.merge(querySreeem, me.internalStreemSubject.asObservable()).debounceTime(10).map(function () {
                         return me.ids.map(function (_id) {
                             return me.docs[_id];
